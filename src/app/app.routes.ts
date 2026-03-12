d0a7c65e06bfd078733d347552e4f51dc854fcfb
Zack2626-ok <thezach2626@gmail.com>
2026-03-12 13:46:38 +0700

    hoan thanh bai tap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { Contact } from './pages/contact/contact';
 import { About } from './about/about';
 import { Products } from './pages/products/products';
+import { ProductDetail } from './pages/product-detail/product-detail';
+
 export const routes: Routes = [
   { path: 'contact', component: Contact },
   { path: 'about', component: About },
   { path: 'products', component: Products },
+  { path: 'products/:slug', component: ProductDetail },
 ];
--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [RouterOutlet, RouterLinkWithHref],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
