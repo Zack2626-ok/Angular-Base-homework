a37fcab9a3fcc82a06e0f8a861883080d53d53dd
Zack2626-ok <thezach2626@gmail.com>
2026-03-12 12:43:41 +0700

    bai tap buoi 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Contact } from './pages/contact/contact';
+import { About } from './about/about';
+import { Products } from './pages/products/products';
+export const routes: Routes = [
+  { path: 'contact', component: Contact },
+  { path: 'about', component: About },
+  { path: 'products', component: Products },
+];
